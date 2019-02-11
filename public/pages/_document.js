65f283eae48ae0154fea4ca57d0bd28b39b088a7
maticzav <matic.zavadlal@gmail.com>
2019-02-11 21:13:56 +0100

    add static files, favicon

--- public/pages/_document.js
+++ public/pages/_document.js
@@ -58,6 +58,32 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/icons/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/icons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/icons/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/static/icons/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />
+
           {this.props.styleTags}
         </Head>
         <body>
