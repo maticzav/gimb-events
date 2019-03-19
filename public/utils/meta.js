0c0e8bd05504672d969060d07c91ab08d59dce6c
maticzav <matic.zavadlal@gmail.com>
2019-03-19 23:34:32 +0100

    add thumbnail

--- public/utils/meta.js
+++ public/utils/meta.js
@@ -1,7 +1,8 @@
 export default {
   url: 'https://dogodki.gimb.xyz/',
   title: 'Gimb Dogodki | Dogodki na Gimnaziji Bežigrad',
-  description: 'Spletna stran dogodkov na Gimnaziji Bežigrad.',
+  description:
+    'Poišči prireditve in dogodke, ki se odvijajo na Gimnaziji Bežigrad, in si zagotovi vstopnice za njihov ogled.',
   keywords: 'Gimnazija, Bežigrad, dogodki, karte, GIMB, prireditve',
   thumbnail: 'https://dogodki.gimb.xyz/static/images/logo_large.png',
   googleSiteVerification: 'KO93L5YzbmvzUbqRYFleIIsINAM2yLacaQfJ9_MtOJg',
